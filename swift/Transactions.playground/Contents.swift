12dfdbdfc18bfb2be52d55a46177fad28da46f6c
Apoo <113794389+Deepak05-09@users.noreply.github.com>
2023-02-03 18:50:33 +0530

    Add files via upload

---
+++ swift/Transactions.playground/Contents.swift
@@ -0,0 +1,320 @@
+import Cocoa
+var val=[
+    [
+      "modified_time" : 1628286549662,
+      "record_id" : nil,
+      "transaction_id" : "NEW1628286549661",
+      "lookup_column" : "SEID_LOOKUP",
+      "table_name" : "TasksRecords_641867043",
+      "lookup_id" : "NEW1628286549659",
+      "lookup_label" : "SEID_LOOKUP"
+    ],
+    [
+      "table_name" : "TasksRecords_641867043",
+      "lookup_label" : "SEID_LOOKUP",
+      "record_id" : nil,
+      "lookup_id" : "NEW1628286549659",
+      "lookup_column" : "SEID_LOOKUP",
+      "modified_time" : 1628286550079,
+      "transaction_id" : "NEW1628286550078"
+    ],
+    [
+      "lookup_label" : "SEID_LOOKUP",
+      "transaction_id" : "NEW1628286550076",
+      "lookup_column" : "SEID_LOOKUP",
+      "record_id" : nil,
+      "table_name" : "TasksRecords_641867043",
+      "lookup_id" : "NEW1628286549659",
+      "modified_time" : 1628286550077
+    ],
+    [
+      "lookup_id" : "NEW1628286551561",
+      "table_name" : "ContactsRecords_641867043",
+      "transaction_id" : "NEW1628286551565",
+      "lookup_label" : "ACCOUNTID_LOOKUP",
+      "record_id" : nil,
+      "modified_time" : 1628286551567,
+      "lookup_column" : "ACCOUNTID_LOOKUP"
+    ],
+    [
+      "table_name" : "EventsRecords_641867043",
+      "transaction_id" : "NEW1628286551568",
+      "lookup_id" : "NEW1628286551561",
+      "record_id" : nil,
+      "lookup_label" : "SEID_LOOKUP",
+      "lookup_column" : "SEID_LOOKUP",
+      "modified_time" : 1628286551569
+    ],
+    [
+      "lookup_id" : "NEW1628286551565",
+      "modified_time" : 1628286551569,
+      "record_id" : nil,
+      "table_name" : "EventsRecords_641867043",
+      "transaction_id" : "NEW1628286551568",
+      "lookup_label" : "CONTACTID_LOOKUP",
+      "lookup_column" : "CONTACTID_LOOKUP"
+    ]
+  ]
+var values = [
+    [
+      "module_name" : "Accounts",
+      "transaction_id" : "NEW1628286549659",
+      "transaction_time" : 1628286549660,
+      "error_message" : "{error=(\"crmDUPLICATE_DATA\",\"duplicatedata\");info={details={\"api_name\"=\"Account_Name\";\"duplicate_record\"={\"Account_Name\"=\"GraniteStateLunchBox\";Owner={id=2366450000004838045;name=\"LindaWilliams\";zuid=693323309;};id=2366450000006996240;};id=2366450000006996240;\"json_path\"=\"$.data[0].Account_Name\";\"more_records\"=0;};module=Accounts;servertime=1628783203274;};}",
+      "operation_type" : 1
+    ],
+    [
+      "transaction_time" : 1628286549662,
+      "operation_type" : 1,
+      "module_name" : "Tasks",
+      "transaction_id" : "NEW1628286549661",
+      "error_message" : ""
+    ],
+    [
+      "module_name" : "Tasks",
+      "operation_type" : 1,
+      "transaction_time" : 1628286550079,
+      "transaction_id" : "NEW1628286550078",
+      "error_message" : ""
+    ],
+    [
+      "error_message" : "",
+      "module_name" : "Tasks",
+      "operation_type" : 1,
+      "transaction_time" : 1628286550077,
+      "transaction_id" : "NEW1628286550076"
+    ],
+    [
+      "operation_type" : 1,
+      "transaction_id" : "NEW1628286550763",
+      "transaction_time" : 1628286550764,
+      "module_name" : "Accounts",
+      "error_message" : "{error=(\"crmDUPLICATE_DATA\",\"duplicatedata\");info={details={\"api_name\"=\"Account_Name\";\"duplicate_record\"={Owner={id=2366450000000166015;name=\"SalesDepartment\";zuid=643501550;};id=2366450000006990259;};id=2366450000006990259;\"json_path\"=\"$.data[0].Account_Name\";\"more_records\"=0;};module=Accounts;servertime=1636640982773;};}"
+    ],
+    [
+      "module_name" : "Accounts",
+      "operation_type" : 1,
+      "error_message" : "{error=(\"crmDUPLICATE_DATA\",\"duplicatedata\");info={details={\"api_name\"=\"Account_Name\";\"duplicate_record\"={Owner={id=2366450000037692018;name=\"RobertNorton\";zuid=749073708;};id=2366450000059074189;};id=2366450000059074189;\"json_path\"=\"$.data[0].Account_Name\";\"more_records\"=0;};module=Accounts;servertime=1657119214448;};}",
+      "transaction_id" : "NEW1628286551561",
+      "transaction_time" : 1628286551562
+    ],
+    [
+      "module_name" : "Contacts",
+      "transaction_id" : "NEW1628286551565",
+      "error_message" : "",
+      "transaction_time" : 1628286551567,
+      "operation_type" : 1
+    ],
+    [
+      "operation_type" : 1,
+      "error_message" : "",
+      "transaction_time" : 1628286551569,
+      "transaction_id" : "NEW1628286551568",
+      "module_name" : "Events"
+    ],
+    [
+      "error_message" : "{error=(\"crmDUPLICATE_DATA\",\"duplicatedata\");info={details={\"api_name\"=\"Account_Name\";\"duplicate_record\"={Owner={id=2366450000004803110;name=\"NickJarvis\";zuid=693114560;};id=2366450000006997722;};id=2366450000006997722;\"json_path\"=\"$.data[0].Account_Name\";\"more_records\"=0;};module=Accounts;servertime=1661983345983;};}",
+      "transaction_time" : 1628286552244,
+      "operation_type" : 1,
+      "transaction_id" : "NEW1628286552243",
+      "module_name" : "Accounts"
+    ],
+    [
+      "transaction_id" : "NEW1628286552249",
+      "module_name" : "Accounts",
+      "error_message" : "{error=(\"crmDUPLICATE_DATA\",\"duplicatedata\");info={details={\"api_name\"=\"Account_Name\";\"duplicate_record\"={Owner={id=2366450000000166015;name=\"SalesDepartment\";zuid=643501550;};id=2366450000062134245;};id=2366450000062134245;\"json_path\"=\"$.data[0].Account_Name\";\"more_records\"=0;};module=Accounts;servertime=1662502894689;};}",
+      "transaction_time" : 1628286552250,
+      "operation_type" : 1
+    ],
+    [
+      "transaction_id" : "NEW1628286552399",
+      "error_message" : "{error=(\"crmDUPLICATE_DATA\",\"duplicatedata\");info={details={\"api_name\"=\"Account_Name\";\"duplicate_record\"={Owner={id=2366450000000166015;name=\"SalesDepartment\";zuid=643501550;};id=2366450000003065168;};id=2366450000003065168;\"json_path\"=\"$.data[0].Account_Name\";\"more_records\"=0;};module=Accounts;servertime=1665087994556;};}",
+      "transaction_time" : 1628286552400,
+      "operation_type" : 1,
+      "module_name" : "Accounts"
+    ],
+    [
+      "module_name" : "Accounts",
+      "transaction_id" : "NEW1628286552709",
+      "operation_type" : 1,
+      "transaction_time" : 1628286552710,
+      "error_message" : "{error=(\"crmDUPLICATE_DATA\",\"duplicatedata\");info={details={\"api_name\"=\"Account_Name\";\"duplicate_record\"={Owner={id=2366450000000166015;name=\"SalesDepartment\";zuid=643501550;};id=2366450000014405182;};id=2366450000014405182;\"json_path\"=\"$.data[0].Account_Name\";\"more_records\"=0;};module=Accounts;servertime=1669406950938;};}"
+    ],
+    [
+      "transaction_id" : "2366450000069084003",
+      "error_message" : "{error=(\"crmINVALID_DATA\",\"theidgivenseemstobeinvalid\");info={details={\"api_name\"=id;\"json_path\"=\"$.data[0].id\";};module=Tasks;servertime=1654045169765;};}",
+      "module_name" : "Tasks",
+      "operation_type" : 2,
+      "transaction_time" : 1628286551119
+    ],
+    [
+      "module_name" : "Tasks",
+      "transaction_time" : 1628286551200,
+      "transaction_id" : "2366450000075309136",
+      "error_message" : "{error=(\"crmINVALID_DATA\",\"theidgivenseemstobeinvalid\");info={details={\"api_name\"=id;\"json_path\"=\"$.data[0].id\";};module=Tasks;servertime=1654712609726;};}",
+      "operation_type" : 2
+    ]
+  ]
+
+
+class Transaction : CustomStringConvertible{
+    let transaction_id:String
+    let module_name:String
+    let transaction_time:String
+    let operation_type:Int
+    let error_message:String
+    
+    init(transaction_id:String,module_name:String,transaction_time:String,operation_type:Int,error_message:String){
+        self.transaction_id=transaction_id
+        self.module_name=module_name
+        self.transaction_time=transaction_time
+        self.operation_type=operation_type
+        self.error_message=error_message
+    }
+    
+    var description: String{
+       return transaction_id+"\t"+module_name+"\t\t\t"+String(operation_type)+"\t\t\t\t"+transaction_time
+    }
+    
+}
+
+class TransactionMap: CustomStringConvertible{
+    let transaction_id:String
+    let lookup_id:String
+    let lookup_column:String
+    let lookup_label:String
+    let record_id:String?
+    let table_name:String
+    let modified_time:String
+    
+    init(transaction_id:String,lookup_id:String,lookup_column:String,
+         lookup_label:String,record_id:String?,table_name:String,modified_time:String) {
+        self.transaction_id=transaction_id
+        self.lookup_id=lookup_id
+        self.lookup_column=lookup_column
+        self.lookup_label=lookup_label
+        self.record_id=record_id
+        self.table_name=table_name
+        self.modified_time=modified_time
+    }
+    
+    var description: String{
+        let id:String
+        if let record_id{
+          id=record_id
+        }
+        else{
+            id="-"
+        }
+       return transaction_id+"\t"+lookup_id+"\t"+lookup_column+"\t\t"+lookup_label+"\t\t\t"+id+"\t\t"+table_name+"\t\t"+modified_time
+    }
+}
+
+enum Sort{
+    case transaction_id,module_name,transaction_time,operation_type
+}
+enum SortMap{
+    case TRANSACTION_ID,LOOKUP_ID,LOOKUP_COLUMN,LOOKUP_LABEL,RECORD_ID,TABLE_NAME,MODIFIED_TIME
+}
+
+func create<T>(array:[[String:Any?]],obj:([String:Any?])->AnyObject)->[T]{
+    
+    var transactions=[T]()
+    
+    for transaction in array{
+        let item=obj(transaction)
+        if( item is T){
+            transactions.append(item as! T)
+        }
+    }
+    return transactions
+}
+
+func sort(array:inout[Transaction] , by type:Sort){
+    switch type{
+    case .transaction_id:
+        array.sort(by: {$0.transaction_id<$1.transaction_id})
+    case .module_name:
+        array.sort(by: {$0.module_name<$1.module_name})
+    case .transaction_time:
+        array.sort(by: {$0.transaction_time<$1.transaction_time})
+    case .operation_type:
+        array.sort(by: {$0.operation_type<$1.operation_type})
+    }
+}
+
+func sortMap(array:inout[TransactionMap],by type:SortMap){
+    switch type{
+    case .TRANSACTION_ID:
+        array.sort(by:{ $0.transaction_id<$1.transaction_id})
+    case .LOOKUP_ID:
+        array.sort(by: {$0.lookup_id<$1.lookup_id})
+    case .LOOKUP_COLUMN:
+        array.sort(by: {$0.lookup_column<$1.lookup_column})
+    case .LOOKUP_LABEL:
+        array.sort(by: {$0.lookup_label<$1.lookup_label})
+    case .RECORD_ID:
+        array.sort(by: {$0.record_id!<$1.record_id!})
+    case .TABLE_NAME:
+        array.sort(by: {$0.table_name<$1.table_name})
+    case .MODIFIED_TIME:
+        array.sort(by: {$0.modified_time<$1.modified_time})
+    }
+}
+
+func Output<T>(text:String ,array:[T]){
+    print(text)
+    for i in 0..<array.count{
+        print("\(i+1)\t\(array[i])")
+    }
+}
+
+var obj:([String:Any])->AnyObject={ transaction in
+    let id=transaction["transaction_id"] as! String
+    let name=transaction["module_name"] as! String
+    
+    let time={
+        let date = Date(timeIntervalSince1970:Double(transaction["transaction_time"] as! Int) / 1000.0)
+
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter.string(from: date)
+    }
+    
+    let op=transaction["operation_type"] as! Int
+    let msg=transaction["error_message"] as! String
+    
+    return Transaction(transaction_id: id, module_name: name, transaction_time:time(), operation_type: op, error_message: msg) as AnyObject
+}
+
+
+var obj2:([String:Any])->AnyObject={ transactionMap in
+    let id=transactionMap["transaction_id"] as! String
+    let l_id=transactionMap["lookup_id"] as! String
+    let l_column=transactionMap["lookup_column"] as! String
+    let l_label=transactionMap["lookup_label"] as! String
+    let record=transactionMap["record_id"] ?? "-"
+    let table=transactionMap["table_name"] as! String
+    let time={
+        let date = Date(timeIntervalSince1970:Double(transactionMap["modified_time"] as! Int) / 1000.0)
+
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter.string(from: date)
+    }
+    return TransactionMap(transaction_id: id, lookup_id: l_id, lookup_column: l_column, lookup_label: l_label, record_id: record as? String  , table_name: table, modified_time: time()) as AnyObject
+}
+
+var text="S.NO\tTRANSACTION ID\tMODULE NAME\t\tOPERATION TYPE\tTRANSACTION TIME"
+var text2="S.NO\tTRANSACTION_ID\tLOOKUP_ID\t\t\tLOOKUP_COLUMN\tLOOKUP_LABEL\tRECORD_ID\tTABLE_NAME\tMODIFIED_TIME"
+
+var transactions:[Transaction]=create(array: values, obj: obj)
+sort(array: &transactions, by: .module_name)
+Output(text: text, array: transactions)
+print("\n")
+var transactionMap:[TransactionMap]=create(array: val , obj: obj2)
+sortMap(array: &transactionMap, by: .TABLE_NAME)
+Output(text: text2, array: transactionMap)
+
